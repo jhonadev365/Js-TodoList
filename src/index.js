3db4e9c60f129122a1c30cda05fa78d82bffc177
jhonadev365 <JhonDev365@gmail.com>
2022-12-20 17:30:22 -0400

    DocsForGitPages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import {saludar} from './js/componentes';
 import './style.css';
 
-const nombre = 'Error ';
+//const nombre = 'Error ';
 
-saludar(nombre);
+const element_btn = document.getElementById("btn_greet");
+const element_input = document.getElementById("usr_name");
+
+element_btn.addEventListener('click', function (){
+	if (element_input.value != '') {
+		saludar(element_input.value);
+	}else{
+		alert('Ingrese un nombre valido...');
+	}
+});
+
